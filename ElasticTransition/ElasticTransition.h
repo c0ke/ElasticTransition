c63c88b47355496bb37b9fde75238a2b3d7bcacc
Jakkapong Yuangtong <visualizeq@gmail.com>
2017-01-31 12:22:21 +0700

    support carthage on swift 3.0.2

---
+++ ElasticTransition/ElasticTransition.h
@@ -0,0 +1,19 @@
+//
+//  ElasticTransition.h
+//  ElasticTransition
+//
+//  Created by Coke on 1/31/17.
+//  Copyright © 2017 Coke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ElasticTransition.
+FOUNDATION_EXPORT double ElasticTransitionVersionNumber;
+
+//! Project version string for ElasticTransition.
+FOUNDATION_EXPORT const unsigned char ElasticTransitionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ElasticTransition/PublicHeader.h>
+
+
